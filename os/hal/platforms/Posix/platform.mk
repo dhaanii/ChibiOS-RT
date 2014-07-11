439f174a4fc28880202deef79cbc0e838b5400d3
Jack <jack@ubuntu.(none)>
2014-07-10 20:51:14 -0700

    updated platform.mk to include SPI

--- os/hal/platforms/Posix/platform.mk
+++ os/hal/platforms/Posix/platform.mk
@@ -3,6 +3,7 @@ PLATFORMSRC = ${CHIBIOS}/os/hal/platforms/Posix/hal_lld.c \
               ${CHIBIOS}/os/hal/platforms/Posix/pal_lld.c \
               ${CHIBIOS}/os/hal/platforms/Posix/serial_lld.c \
               ${CHIBIOS}/os/hal/platforms/Posix/ext_lld.c \
+              ${CHIBIOS}/os/hal/platforms/Posix/spi_lld.c \
 
 # Required include directories
 PLATFORMINC = ${CHIBIOS}/os/hal/platforms/Posix
