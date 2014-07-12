7cceacfdbc338daa2dc33754362f960666d4330c
AMBATEMAN <bateman3@pdx.edu>
2014-07-11 18:46:58 -0700

    ADDED RTC

---
+++ demos/Posix-GCC/.dep/memstreams.o.d
@@ -0,0 +1,127 @@
+../../os/various/memstreams.o: ../../os/various/memstreams.c \
+ /usr/include/stdc-predef.h /usr/include/string.h /usr/include/features.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/wordsize.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-32.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/stddef.h \
+ /usr/include/xlocale.h /usr/include/bits/string.h \
+ /usr/include/bits/string2.h /usr/include/endian.h \
+ /usr/include/bits/endian.h /usr/include/bits/byteswap.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/bits/byteswap-16.h /usr/include/stdlib.h \
+ /usr/include/bits/string3.h ../../os/kernel/include/ch.h chconf.h \
+ ../../os/ports/GCC/SIMIA32/chtypes.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/stdint.h /usr/include/stdint.h \
+ /usr/include/bits/wchar.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/stdbool.h \
+ ../../os/kernel/include/chlists.h ../../os/ports/GCC/SIMIA32/chcore.h \
+ ../../os/kernel/include/chsys.h ../../os/kernel/include/chvt.h \
+ ../../os/kernel/include/chschd.h ../../os/kernel/include/chsem.h \
+ ../../os/kernel/include/chbsem.h ../../os/kernel/include/chmtx.h \
+ ../../os/kernel/include/chcond.h ../../os/kernel/include/chevents.h \
+ ../../os/kernel/include/chmsg.h ../../os/kernel/include/chmboxes.h \
+ ../../os/kernel/include/chmemcore.h ../../os/kernel/include/chheap.h \
+ ../../os/kernel/include/chmempools.h ../../os/kernel/include/chthreads.h \
+ ../../os/kernel/include/chdynamic.h ../../os/kernel/include/chregistry.h \
+ ../../os/kernel/include/chinline.h ../../os/kernel/include/chqueues.h \
+ ../../os/kernel/include/chstreams.h ../../os/kernel/include/chfiles.h \
+ ../../os/kernel/include/chdebug.h ../../os/various/memstreams.h
+
+/usr/include/stdc-predef.h:
+
+/usr/include/string.h:
+
+/usr/include/features.h:
+
+/usr/include/sys/cdefs.h:
+
+/usr/include/bits/wordsize.h:
+
+/usr/include/gnu/stubs.h:
+
+/usr/include/gnu/stubs-32.h:
+
+/usr/lib/gcc/x86_64-linux-gnu/4.8/include/stddef.h:
+
+/usr/include/xlocale.h:
+
+/usr/include/bits/string.h:
+
+/usr/include/bits/string2.h:
+
+/usr/include/endian.h:
+
+/usr/include/bits/endian.h:
+
+/usr/include/bits/byteswap.h:
+
+/usr/include/bits/types.h:
+
+/usr/include/bits/typesizes.h:
+
+/usr/include/bits/byteswap-16.h:
+
+/usr/include/stdlib.h:
+
+/usr/include/bits/string3.h:
+
+../../os/kernel/include/ch.h:
+
+chconf.h:
+
+../../os/ports/GCC/SIMIA32/chtypes.h:
+
+/usr/lib/gcc/x86_64-linux-gnu/4.8/include/stdint.h:
+
+/usr/include/stdint.h:
+
+/usr/include/bits/wchar.h:
+
+/usr/lib/gcc/x86_64-linux-gnu/4.8/include/stdbool.h:
+
+../../os/kernel/include/chlists.h:
+
+../../os/ports/GCC/SIMIA32/chcore.h:
+
+../../os/kernel/include/chsys.h:
+
+../../os/kernel/include/chvt.h:
+
+../../os/kernel/include/chschd.h:
+
+../../os/kernel/include/chsem.h:
+
+../../os/kernel/include/chbsem.h:
+
+../../os/kernel/include/chmtx.h:
+
+../../os/kernel/include/chcond.h:
+
+../../os/kernel/include/chevents.h:
+
+../../os/kernel/include/chmsg.h:
+
+../../os/kernel/include/chmboxes.h:
+
+../../os/kernel/include/chmemcore.h:
+
+../../os/kernel/include/chheap.h:
+
+../../os/kernel/include/chmempools.h:
+
+../../os/kernel/include/chthreads.h:
+
+../../os/kernel/include/chdynamic.h:
+
+../../os/kernel/include/chregistry.h:
+
+../../os/kernel/include/chinline.h:
+
+../../os/kernel/include/chqueues.h:
+
+../../os/kernel/include/chstreams.h:
+
+../../os/kernel/include/chfiles.h:
+
+../../os/kernel/include/chdebug.h:
+
+../../os/various/memstreams.h:
